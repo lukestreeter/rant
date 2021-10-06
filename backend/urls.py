93f76610bfa5291832a43bb6049f6fd615cc48f2
lukestreeter <lukecharlesstreeter@gmail.com>
2021-10-06 13:24:50 -0500

    lecture 146 done about Template Fillters

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,5 @@ app_name = 'basic_app'
 
 urlpatterns= [
     url(r'^relative/$',views.relative,name='relative'),
-    url(r'^other/$',views.other,name='other'),
+    url(r'^other/$',views.other,name='other'),S
 ]
