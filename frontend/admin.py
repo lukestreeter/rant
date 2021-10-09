f1d6639ad3de96e9ae54d91686432cec60b257b2
lukestreeter <lukecharlesstreeter@gmail.com>
2021-10-08 19:32:53 -0500

    lecture 151 done obout Coding User Model exit clear wuit quit
    help

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from appTwo.models import User
+from basic_app.models import UserProfileInfo
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Topic)
-admin.stie.rigister(Webpage)
+admin.site.register(UserProfileInfo)
